d274c240b28d85e41c63befb09285e134dc93794
Valentin <valio990210@gmail.com>
2023-02-14 17:20:20 +0200

    .

--- 02.HTTP-Status-Cats/app.js
+++ 02.HTTP-Status-Cats/app.js
@@ -13,9 +13,9 @@ const catTemplate = html`
     function createCatCard(cat){
         return html`
                     <li>
-                <img src="./images/${cat.imageLocation}" width="250" height="250" alt="Card image cap">
+                <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
                 <div class="info">
-                    <button @click=${showContent}class="showBtn">Show status code</button>
+                    <button @click="${showContent}" class="showBtn">Show status code</button>
                     <div class="status" style="display: none" id="${cat.id}">
                         <h4>Status Code: ${cat.statusCode}</h4>
                         <p>${cat.statusMessage}</p>
