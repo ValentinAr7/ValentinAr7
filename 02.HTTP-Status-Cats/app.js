7cfa5717a594359886d2b2a425511c186d62c42b
Valentin <valio990210@gmail.com>
2023-02-14 17:10:04 +0200

    html

--- 02.HTTP-Status-Cats/app.js
+++ 02.HTTP-Status-Cats/app.js
@@ -7,3 +7,26 @@ const catTemplate = html`
     <ul>
         ${cats.map(cat => createCatCard(cat))}
     </ul>`
+
+    render (catTemplate, root)
+
+    function createCatCard(cat){
+        return html`
+                    <li>
+                <img src="./images/${cat.imageLocation}" width="250" height="250" alt="Card image cap">
+                <div class="info">
+                    <button @click=${showContent}class="showBtn">Show status code</button>
+                    <div class="status" style="display: none" id="${cat.id}">
+                        <h4>Status Code: ${cat.statusCode}</h4>
+                        <p>${cat.statusMessage}</p>
+                    </div>
+                </div>
+            </li>`
+    }
+
+    function showContent(e){
+        const contentContainer = e.target.parentElement.querySelector('div');
+        const currentState = contentContainer.style.display;
+
+        if(currentState === 'none')
+    }
