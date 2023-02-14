e47022122056c2fe9cfcf907de3d6f405fb85477
Valentin <valio990210@gmail.com>
2023-02-14 16:59:19 +0200

    for each cat create card

--- 02.HTTP-Status-Cats/app.js
+++ 02.HTTP-Status-Cats/app.js
@@ -1,2 +1,9 @@
-import {html, render} from './node_modules/lit-html/lit-html.js';
-import {cats} from './catSeeder.js'
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js'
+
+const root = document.getElementById('allCats')
+
+const catTemplate = html`
+    <ul>
+        ${cats.map(cat => createCatCard(cat))}
+    </ul>`
