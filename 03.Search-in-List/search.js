7d57161441b61972e70dbc7ee32bda63be2cb392
Valentin <valio990210@gmail.com>
2023-02-15 13:47:34 +0200

    return townsas li elements

--- 03.Search-in-List/search.js
+++ 03.Search-in-List/search.js
@@ -1,6 +1,21 @@
 import {html, render} from './node_modules/lit-html/lit-html.js'
 import {towns} from './towns.js'
 
+const townsRoot =document.getElementById('towns')
+const resultRoot = document.getElementById('result')
+
+function searchTemplate(townsName){
+   const ul = html`
+   <ul>
+   ${towns.map(town => createLiTemplate(town))}
+   </ul>`
+}
+
+function createLiTemplate(town){
+   return html`
+   <li>${town}</li>`
+}
+
 function search() {
  
 }
